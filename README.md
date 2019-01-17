6bf5c0c0408982698bd944daede62b3bdcf2b85c
Khai Do <zaro0508@gmail.com>
2019-01-17 09:14:41 -0800

    [Resolve #228] Sceptre stack termination protection hook
    This is a Sceptre hook to allow sceptre to enable and disable
    cloudformation stack termination protection.
    
    sceptre issue: https://github.com/cloudreach/sceptre/issues/228

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# sceptre-stack-termination-protection-hook
-A Sceptre hook to enable and disable stack termination protection
+---
+layout: docs
+title: Hooks
+---
+
+# Overview
+
+The purpose of this hook is to enable and disable the stack
+termination protection. 
+
+## Available Hooks
+
+### StackTermination
+
+Enables and disables stack termination.
+
+Syntax:
+
+```yaml
+parameter|sceptre_user_data:
+    <name>: !stack_termination_protection 'enabled'/'disabled'
+```
+
+Example:
+
+Enable stack termination protection after creating stack
+and disable stack termination protection before stack deletion:
+```
+hooks:
+  after_create:
+    - !stack_termination_protection 'enabled'
+  before_delete:
+    - !stack_termination_protection 'disabled'
+```
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+install_requirements = [
+    "sceptre>=2.0"
+]
+
+setup(
+    name='sceptre-stack-termination-protection-hook',
+    version="1.0.0",
+    description="A Sceptre hook to enable and disable cloudformation"
+                "stack termination protection",
+    py_modules=['stack_termination_protection'],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="zaro0508",
+    author_email="zaro0508@gmail.com",
+    license='Apache2',
+    url="https://github.com/cloudreach/sceptre",
+    entry_points={
+        'sceptre.hooks': [
+            'stack_termination_protection = '
+            'stack_termination_protection:StackTerminationProtection',
+        ],
+    },
+    keywords="sceptre",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Environment :: Console",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    ],
+    install_requires=install_requirements,
+    include_package_data=True,
+    zip_safe=False,
+)
